a37039a76ced4ce56c2b9aacf13a5813e690a901
QuispeJoseDavid <130864498+JoseDavidFull@users.noreply.github.com>
2023-12-23 12:17:56 -0600

    new datos

--- DesarrolloWeb/mi_primer_vue/src/App.vue
+++ DesarrolloWeb/mi_primer_vue/src/App.vue
@@ -1,10 +1,28 @@
 <script setup>
+/*Crea una lista de objetos en Vue.js.
+Implementa botones de "Agregar" y "Eliminar" para agregar
+ y quitar elementos de la lista dinámicamente. */
+import { ref } from 'vue';
+
+/*splice,1 */
+
+const lista = ref([])
+
+
+const agregar  =() =>{
+    lista.value.push({(lista.)})
+}
 
 
 </script>
+
+
 <template>
 
 
 </template>
 
 
+<style>
+
+</style>
