c7ce2ba67b2ac979d6a9a42a4f408c4a79d10087
QuispeJoseDavid <130864498+JoseDavidFull@users.noreply.github.com>
2024-01-23 14:43:36 -0600

    routersin funcionamiento

--- DesarrolloWeb/mi_primer_vue/src/components/Hijo.vue
+++ DesarrolloWeb/mi_primer_vue/src/components/Hijo.vue
@@ -1,28 +1,10 @@
 <script setup>
-/*Lista de Tareas:
-Crea una aplicación de lista de tareas. Permite agregar, marcar como completadas y eliminar tareas. */
-
-const Tareas = [
-    { name: "Tener Cita Con Bertha", pendiente: 1 },
-    { name: "Tener Sexo Con Bertha", pendiente: 0 },
-    { name: "Dejarla Embarazada", pendiente: 1 },
-    { name: "Comprar Ingredientes para la Cena", pendiente: 1 },
-    { name: "Leer un Libro", pendiente: 0 },
-];
-
-
 
 </script>
 
 <template>
-    <ul>
-        <li v-for="index in Tareas" :key="index.name">
-            {{ index.name }}
-        </li>
-    </ul>
     
 </template>
-
 
 <style scoped>
 
---
+++ DesarrolloWeb/mi_primer_vue/src/views/Brazil.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="brazil">
+        <p>Por qué rayos no aparece</p>
+    </div>
+</template>
+
+<script>
+</script>
